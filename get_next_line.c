0be884098d9523b0c19362829e30f2e1c4572369
Oleh ANTONENKO <oantonen@e1r12p1.unit.ua>
2017-12-03 21:33:45 +0200

    first commit

---
+++ get_next_line.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oantonen <oantonen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/02 20:16:45 by oantonen          #+#    #+#             */
+/*   Updated: 2017/12/03 21:31:26 by oantonen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <string.h>
+
+// #include <stdlib.h>
+// #include <fcntl.h>
+// #include <unistd.h>
+
+char	*ft_strncpy(char *dst, const char *src, size_t len)
+{
+	char *ptr;
+
+	ptr = dst;
+	while (len)
+	{
+		if (!*src)
+			break ;
+		else
+			*ptr++ = *src++;
+		len--;
+	}
+	while (len)
+	{
+		*ptr++ = '\0';
+		len--;
+	}
+	return (dst);
+}
+
+int get_next_line(const int fd, char **line)
+{
+	int		len;
+	char	*tmp;
+
+	len = 0;
+	if (!(tmp = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
+		return (0);
+	if (read(fd, tmp, BUFF_SIZE) == -1)
+		return (-1);
+	tmp[BUFF_SIZE] = '\0';
+	while (*tmp && *tmp++ != '\n')
+		len++;
+	if (len > BUFF_SIZE)
+		read(fd, tmp, BUFF_SIZE)
+	*line = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1));
+	tmp = tmp - len - 1;
+	ft_strncpy(*line, tmp, len);
+	return (1);
+}
+
+int main()
+{
+	int fp;
+	char *line;
+
+	// line = NULL;
+
+	fp = open(".//test.c", O_RDONLY);
+	// line = (char*)malloc(sizeof(char) * 1500);
+	get_next_line(fp, &line);
+	printf("%s", line);
+	// printf("%s\n", &line[0]);
+	// printf("%s\n", &line[1]);
+
+	return (0);
+
+}
---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oantonen <oantonen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/02 20:19:34 by oantonen          #+#    #+#             */
+/*   Updated: 2017/12/03 14:46:51 by oantonen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#define BUFF_SIZE 32
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+#endif
